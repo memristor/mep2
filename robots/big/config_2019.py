45ba2d2b015ffceacbfbea0af0ff99278266a15f
Milos Arbanas <milostravian2@gmail.com>
2020-02-26 23:58:52 +0100

    New config for Big robot in 2020.
    Added testing task

--- robots/big/config_2019.py
+++ robots/big/config_2019.py
@@ -9,10 +9,6 @@ from core.network.Splitter import *
 _core.debug = 0
 
 
-######### Pathfinding #########
-from modules.default_config import pathfind
-###############################
-
 ##### Motion #######
 from modules.default_config import motion, motion_fix
 motion.can.iface = 'can0' 
@@ -63,30 +59,30 @@ def pump(x, v):
 ################### SERVOS #######################
 servo_id = 0x80008d00
 spl = Splitter(can.get_packet_stream(servo_id))
-servo_lok  = Servo('servo_lok', servo_id=22, packet_stream=spl.get())
-servo_rok = Servo('servo_rok', servo_id=21, packet_stream=spl.get())
-servo_lfliper = Servo('servo_lfliper', servo_id=25, packet_stream=spl.get())
-servo_rfliper = Servo('servo_rfliper', servo_id=2, packet_stream=spl.get()) 
+servo_rlift  = Servo('servo_rlift', servo_id=25, packet_stream=spl.get()) # izvuceno 211; medjupozicija 465; uvucano 537
+servo_llift = Servo('servo_llift', servo_id=24, packet_stream=spl.get()) # uvuceno 459; izv 843; medju 577
+servo_lfliper = Servo('servo_lfliper', servo_id=23, packet_stream=spl.get()) # 
+servo_rfliper = Servo('servo_rfliper', servo_id=22, packet_stream=spl.get()) # 
 
 # export servo commands
-servos = [servo_lok, servo_rok, servo_lfliper, servo_rfliper]
+servos = [servo_lfliper, servo_rfliper, servo_rlift, servo_llift]
 for i in servos: i.export_cmds(i.name)
 _core.add_module(servos)
 
 @_core.export_cmd
 @_core.do
-def rok_visina(v):
-	_e.servo_rok.action('GoalPosition', [537, 465, 211][v])
+def rlift(v):
+	_e.servo_rlift.action('GoalPosition', [537, 465, 211][v])
 
 @_core.export_cmd
 @_core.do
-def lok_visina(v):
-	_e.servo_lok.action('GoalPosition', [537, 465, 211][v])
+def llift(v):
+	_e.servo_llift.action('GoalPosition', [459, 577, 843][v])
 
 @_core.export_cmd
 @_core.do
 def lfliper(v):
-	_e.servo_lfliper.action('GoalPosition', [682, 389, 239][v])
+	_e.servo_lfliper.action('GoalPosition', [682, 389, 239][v])  #537, 465, 211
 
 @_core.export_cmd
 @_core.do
@@ -96,22 +92,62 @@ def rfliper(v):
 
 
 ######## STATIC OBSTACLES #######
-_core.entities.add_entity('static', 'zuta_kamenje', [[600, 850], [580, 850], [580, 1000], [600, 1000]])
-_core.entities.add_entity('static', 'plava_kamenje', [[-600, 850], [-620, 850], [-620, 1000], [-600, 1000]])
-_core.entities.add_entity('static', 'sredina_kamenje', [[0, 700], [20, 700], [20, 1000], [0, 1000]])
+_core.entities.add_entity('static', 'rampa', [[-1050,500], [-35,500], [-35, 368], [45,352], [41, 536], [1055,516], [1089, 992], [-1041, 964], [-1050, 500]])
+_core.entities.add_entity('static', 'accelerator', [[-999,-932], [991, -944], [991, -1000], [-991,-1000]])
 #################################
 
 
 ######### Pressure Sensors ############
-pressure = [ 
-	PressureSensor('sp_left', 1, can.get_packet_stream(0x80007801, 0x80007800)),
-	PressureSensor('sp_right', 2, can.get_packet_stream(0x80007802, 0x80007800))
-]
+pressure = [ PressureSensor('pressure'+str(i), i, can.get_packet_stream(0x80007800+i, 0x80007800)) for i in range(9) ]
 # export pressure sensor commands
 _core.add_module(pressure)
 for i in pressure: i.export_cmds(i.name)
 
+@_core.export_cmd
+def pressure(i): # i - pump num
+	i2 = [0, 3, 4, 1, 8, 2, 0, 8, 2, 0][i % 10]
+	a = getattr(_e, 'pressure%d' % i2).picked()
+	return a
 #######################################
+
+######################## LIFT ###################################
+from modules.drivers.motion.Motion import *
+lift_drv = Motion(name='lift', packet_stream=can.get_packet_stream(0x80000259))
+lift_drv.export_cmds('lift_drv')
+_core.add_module(lift_drv)
+
+lift_positions1 = {
+	'accel': 50000+77000+15000,
+	'goldenium': 400000+77000+15000,
+	'pri_vrhu': 1700000+77000+15000,
+	'sredina': 1977000+15000, #1 860 000
+	'dole': 1920000 + 8000*11 + 4000
+}
+
+lift_positions2 = {
+	'accel': 50000,
+	'goldenium': 400000,
+	'pri_vrhu': 1700000,
+	'sredina': 1800000, #1 860 000
+	'dole': 1880000
+}
+
+lift_drv.set_lift_positions(1, lift_positions1)
+lift_drv.set_lift_positions(2, lift_positions2)
+
+@_core.do
+def init_lift():
+	lift_drv.conf_set('init_dir1', -1)
+	lift_drv.conf_set('init_dir2', -1)
+	lift_drv.conf_set('debug_encoders', 0)
+	lift_drv.conf_set('speed1', 300)# bilo 100
+	lift_drv.conf_set('speed2', 300)#bilo 100
+	_e.lift_drv.prepare_lift()
+	_e.lift_drv.conf_set('speed1', 450)# bilo 100
+	_e.lift_drv.conf_set('speed2', 450)#bilo 100
+
+_core.export_cmd('lift', lift_drv.lift)
+#################################################################
 
 
 
@@ -127,41 +163,56 @@ from modules.default_config import lcd
 ###########################
 
 
-############### Coordinates ################# Definisane tacke
+############### Coordinates ################
 @_core.export_cmd
 def coord(c):
-	plava = {
-		'vetrokazi': (1360, 850),
-		'nase_case' : (1360, 720),
-		'srednje_blize': (700, -850),
-		'srednje_dalje': (-700, -850),
-		'mala_luka': (-300, 600),
-		'velika_luka': (1000, -200),
-		'marina_sever': (1300, -750),
-		'marina_jug': (1300, 350),
-		'aktivacija_tornja': (1200, -850)
-
-	}
 	zuta = {
-		'vetrokazi': (-1360, 850),
-		'nase_case' : (-1360, 720),
-		'srednje_blize': (-700, -850),
-		'srednje_dalje': (700, -850),
-		'mala_luka': (300, 600),
-		'velika_luka': (-1000, -200),
-		'marina_sever': (-1300, -750),
-		'marina_jug': (-1300, 350),
-		'aktivacija_tornja': (-1200, -850)
-
+		'prilaz_rampi': (1265,500),
+		
+		'start_plavi' :	(1250-50,110),
+		'start_zeleni': (1250-50,-390),
+		'start_crveni': (1250-50,-520),
+		
+		'slot_2_1': (795,355+15-5-6+8-5-2),
+		'slot_2_1_obratno': (795+4,355+15-5-6+7-5-4-2),
+		'slot_2_2': (698-2,355+15-5-6+7-5-2-1+2+2-4+2+2),
+		'slot_2_2_obratno': (698+2+200,355+15-5-6+7-5-4-7+2+2),
+		'slot_2_poslednje': (698+2+300,355+15-5-6+7-5-4-7+2+2),
+		
+		'priprema_akcelerator_1': (0,-550),
+		'priprema_akcelerator_2_1': (80,-788),
+		'priprema_akcelerator_2_2': (-100,-782),
+	}
+	ljubicasta = {
+		'prilaz_rampi': (-1265,500),
+		
+		'start_plavi': (-1250+50,110),#bilo 1180
+		'start_zeleni': (-1250+50,-390),
+		'start_crveni': (-1250+50,-520),
+	
+		'slot_2_1': (-795,355+15-5-6+8-5-2),
+		'slot_2_1_obratno': (-795-4,355+15-5-6+7-5-4-2),
+		'slot_2_2': (-698+2,355+15-5-6+7-5-2-1+2+2-4+2+2),
+		'slot_2_2_obratno': (-698-2-200,355+15-5-6+7-5-4-7+2+2),
+		'slot_2_poslednje':(-698-2-300,355+15-5-6+7-5-4-7+2+2),
+	
+		'priprema_akcelerator_1': (0,-550),
+		'priprema_akcelerator_2_1': (-80,-788),
+		'priprema_akcelerator_2_2': (100,-782),
 	}
 		
 	if State.color == 'zuta':
 		return zuta[c]
 	else:
-		return plava[c]
+		return ljubicasta[c]
 ############################################
 
 ######## Point Constants ##########
+State.color_vaga_bodovi = {'crveni': 4, 'plavi': 12, 'zeleni': 8, False: 0}
+State.color_elem_bodovi = {'crveni': 6, 'plavi': 6, 'zeleni': 6, False: 0}
+State.akcelerator_bodovi = 10
+State.startno_polje_bodovi = 1
+State.startno_polje_bonus = 5
 ###################################
 
 
@@ -194,8 +245,8 @@ def experiment(v):
 
 @_core.do
 def init_servos():
-	servo_lok.action('Speed',200)
-	servo_rok.action('Speed', 200)
+	servo_rlift.action('Speed',500)
+	servo_llift.action('Speed', 500)
 	servo_lfliper.action('Speed', 200)
 	servo_rfliper.action('Speed', 200)
 
@@ -224,6 +275,9 @@ def init_task():
 	_e.check_chinch()
 	
 	init_servos()
+	_e.rlift(0)
+	_e.llift(0)
+	init_lift()
 	
 	init_pids()
 	_e.r.send('R')
@@ -234,3 +288,4 @@ def init_task():
 	_e.r.conf_set('alpha', 1000)
 	_e.chinch()
 	timer.start_timer()
+	_e.experiment('H')
